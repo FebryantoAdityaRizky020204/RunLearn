c2070a082e47859ec43845c31ea03f3bb2a87c1c
ADITYA_RIZKY_F <febryantoadityarizky@gmail.com>
2025-01-14 21:48:42 +0700

    [some] belajar javascript, end today

--- Javascript/id.javascript.info/DataTypes/ArraysMethod.js
+++ Javascript/id.javascript.info/DataTypes/ArraysMethod.js
@@ -183,6 +183,116 @@ let someUsers = users2.filter(item => item.id < 3);
 console.log(someUsers);
 
 // TODO - mengubah array
+console.log('====================================');
+
+/**
+ * ? map
+ * 
+ * * Metode arr.map adalah salah satu metode yang paling berguna dan paling sering digunakan.
+ * * Metode ini memanggil fungsi untuk tiap elemen di array dan mengembalikan hasilnya dalam bentuk array.
+ */
+
+{
+  let result = ['halo', 'semua'].map(function(item, index, array) {
+  // mengembalikan nilai baru, bukan item
+  });
+}
+// ? contohnya, map akan membuat nilai baru berdasarkan item dalam array
+let lengths = ["Bilbo", "Gandalf", "Nazgul"].map(item => item.length);
+console.log(lengths);
+
+// ? arr.sort() agak ribet, baca disini [https://id.javascript.info/array-methods]
+
+// ? reverse
+// * Metode arr.reverse membalikkan urutan elemen di dalam arr.
+let arr9 = [1, 2, 3, 4, 5];
+arr9.reverse();
+
+console.log(arr9); // [5,4,3,2,1]
+
+// ? split dan join
+
+/**
+ * ? SPLIT
+ * 
+ * * Metode str.split(delimiter) melakukan tepat hal yang dijelaskan di atas. 
+ * * -> Metode ini memisahkan string ke dalam array dengan delimiter (pemisah) delim
+ */
+
+let names = 'Aditya, Rizky, Febryanto'
+let arrNames = names.split(', ')
+console.log(arrNames); // * ['Aditya', 'Rizky', 'Febryanto']
+
+/**
+ * ? JOIN
+ * 
+ * * arr.join(glue) melalukan kebalikan split. ini membuat sebuah string item-item arr digabungkan oleh 
+ * * -> glue (lem) diantara item tersebut.
+ */
+let arrNames2 = arrNames.join(';')
+console.log(arrNames2); // * 'Aditya;Rizky;Febryanto'
+
+// ? reduce , reduceRight
+console.log('====================================');
+
+/**
+ * ? Ketika kita perlu mengulang-ulang sebuah array – kita dapat menggunakan forEach, for atau for..of.
+ * ? Ketika kita perlu mengulang dan mengembalikan data setiap elemen – kita menggunakan map.
+ * 
+ * ? Metode arr.reduce dan metode arr.reduceRight juga termasuk ke dalam kelompok metode-metode tadi, 
+ * ? -> tapi ada sedikit lebih rumit. Kedua metode ini digunakan untuk menghitung sebuah nilai tunggal 
+ * ? -> berdasarkan array.
+ */
+
+{
+  let initial = 0;
+  let value = ['halo', 'semua'].reduce(function(accumulator, item, index, array) {
+    // ...
+  }, [initial]);
+
+  /**
+   * ? Fungsi di atas diterapkan pada semua elemen array satu sama lainnya dan “melanjutkan” hasil perhitungannya 
+   * ? -> ke panggilan berikutnya.
+   * * Argument-argumennya yakni:
+   * * [1] previousValue – adalah hasil dari dari pemanggilan fungsi sebelumnya, sama dengan initial pertama kalinya 
+   * * -> (jika initial diberikan).
+   * * [2] item – adalah item array yang sekarang.
+   * * [3] index – adalah posisi item tersebut.
+   * * [4] array – adalah array-nya.
+   * 
+   * ? Jika fungsi sudah diterapkan, maka hasil dari panggilan fungsi sebelumnya dioper ke panggilan selanjutnya 
+   * ? -> sebagai argumen pertama.
+   */
+}
+
+let arr10 = [1, 2, 3, 4, 5];
+let resultArr10 = arr10.reduce((sum, current) => sum + current, 0)
+console.log(resultArr10);
+
+// ? ribet baca sendiri di link [https://id.javascript.info/array-methods#mengubah-array]
+
+
+// TODO - Array.isArray()
+
+/**
+ * ? Array tidak membentuk sebuah bahasa tipe sendiri. Array terbentuk berdasarkan objek.
+ * ? Jadi typeof tidak membantu membedakan sebuah objek polos dari sebuah array:
+ */
+
+console.log('====================================');
+console.log(typeof { nama: 'Aditya' }); // ? object
+console.log(typeof ['Aditya']); // ? object
+
+/**
+ * ? …Tetapi array serung diguakan hingga ada metode khusus untuk hal ini: Array.isArray(value). 
+ * ? -> Metode ini mengembalikan true jika value adalah sebuah array, dan false jika sebaliknya.
+ */
+
+console.log(Array.isArray({ nama: 'Aditya' })); // ? false
+console.log(Array.isArray(['Aditya'])); // ? true
+
+// TODO - thisArg
+// ? agak membingungkan baca disini [https://id.javascript.info/array-methods#kebanyakan-metode-mendukung-thisarg]
 
 
 
