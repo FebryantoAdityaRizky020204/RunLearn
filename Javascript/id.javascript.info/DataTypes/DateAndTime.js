98a08234e19b24a96f73aeb5dcd9f3b83c1fa4fa
ADITYA_RIZKY_F <febryantoadityarizky@gmail.com>
2025-01-21 22:50:15 +0700

    [some] belajar javascript, again

---
+++ Javascript/id.javascript.info/DataTypes/DateAndTime.js
@@ -0,0 +1,8 @@
+// TODO - Date and Time
+
+// ? untuk membuat date menggunakan new Date()
+let timeNow = new Date(24 * 3600 * 1000)
+console.log(timeNow); // ? akan menampilkan tanggal dan waktu saat ini
+
+// ? agak ribet baca saja disini [https://id.javascript.info/date]
+
