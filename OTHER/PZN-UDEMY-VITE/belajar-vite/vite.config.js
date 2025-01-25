cb5a4db0af44e8a12004e1aaede931c68f861aa7
ADITYA_RIZKY_F <febryantoadityarizky@gmail.com>
2025-01-25 23:00:03 +0700

    [some] belajar vite, udemy-pzn, end chap1

--- OTHER/PZN-UDEMY-VITE/belajar-vite/vite.config.js
+++ OTHER/PZN-UDEMY-VITE/belajar-vite/vite.config.js
@@ -2,7 +2,14 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     build: {
-        outDir: 'production' // ? mengubah nama filder build dari dist menjadi production
+        outDir: 'production', // ? mengubah nama filder build dari dist menjadi production
+        rollupOptions: {
+            input: { // ? menambahkan multi-page app
+                index: 'index.html', // ? defaultnya cuman ini
+                about: 'about.html', // ? untuk halaman about
+                contact: 'other/contact.html' // ? untuk halaman contact
+            }
+        }
     },
     server: {
         port: 5173 // ? mengubah port menjadi localhost:3000
